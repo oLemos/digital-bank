6919a07094baff04438f8923e171d88f518518f2
oLemos <lemmos@outlook.com.br>
2022-03-05 22:15:46 -0300

    login form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,63 @@
+import { createRef, FormEvent } from 'react';
+import * as yup from 'yup';
+import { FormContainer } from './styles';
+
 function Login() {
+    const agencyRef = createRef<HTMLInputElement>();
+    const accountRef = createRef<HTMLInputElement>();
+    const passwordRef = createRef<HTMLInputElement>();
+
+    function logInSubmit(event: FormEvent): void {
+        // Para previnir o recarregamento descenssário da página.
+        event.preventDefault();
+
+        const userInfo = {
+            agency: agencyRef.current?.value,
+            account: accountRef.current?.value,
+            password: passwordRef.current?.value,
+        };
+
+        const schema = yup.object().shape({
+            agency: yup.number().required(),
+            account: yup.number().required(),
+            password: yup.string().required(),
+        });
+
+        schema
+            .validate(userInfo, { abortEarly: false })
+            .then((response) => {
+                // Ajuste -> Requisição de login
+                console.log(response);
+            })
+            .catch((error) => {
+                throw new Error(error);
+            });
+    }
+
     return (
-        <form action="">
-            <input placeholder="Agência" />
-            <input placeholder="Conta" />
-            <input type="password" name="passwordInput" id="passwordInput" />
+        <FormContainer onSubmit={logInSubmit}>
+            <input
+                ref={agencyRef}
+                placeholder="Agência"
+                required
+                type="number"
+            />
+            <input
+                ref={accountRef}
+                placeholder="Conta"
+                required
+                type="number"
+            />
+            <input
+                ref={passwordRef}
+                placeholder="Senha eletrônica"
+                type="password"
+                required
+            />
             <button type="submit">
                 Entrar
             </button>
-        </form>
+        </FormContainer>
     );
 }
 
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.form`
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type='number'] {
+        -moz-appearance: textfield;
+    }
+`;
